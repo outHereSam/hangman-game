4d2846b7e7b6affd205111ab4459ca19c14555bf
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-07-09 10:33:28 +0000

    Implements design for how to play page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { HowToPlayComponent } from './components/how-to-play/how-to-play.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'how-to-play', component: HowToPlayComponent },
+];
---
+++ src/app/components/how-to-play/how-to-play.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-how-to-play',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './how-to-play.component.html',
+  styleUrl: './how-to-play.component.css',
+})
+export class HowToPlayComponent {}
